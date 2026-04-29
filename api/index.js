620b50f98caf2a7b07cafc1bc1299caa4a71a360
ilafper <ivanaalfaya@gmail.com>
2026-04-29 17:15:06 +0200

    cambio de app.listen

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
 const app = require('./app');
 
 const port = 3000;
-
 app.listen(port, "0.0.0.0", () => {
   console.log("Listening on port 3000");
-  console.log("http://10.0.2.2:3000");
-  
+
 });
